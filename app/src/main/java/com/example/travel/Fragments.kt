157407787d9edb66039183cd544f5ea4fa047a3c
Dasol <ekthf2013@naver.com>
2023-08-03 00:58:04 +0900

    Tab_bar
    추가 진행중

---
+++ app/src/main/java/com/example/travel/Fragments.kt
@@ -0,0 +1,15 @@
+package com.example.travel
+
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment(R.layout.main_page) {
+
+}
+
+class RouteFragment : Fragment(R.layout.recom_route_page){
+
+}
+
+class MypageFragment : Fragment(R.layout.mypage){
+
+}
